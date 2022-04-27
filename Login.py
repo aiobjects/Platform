1f05a9e8ee73a803d292f3a4a274033938182a59
adityaabhinav2 <91604826+adityaabhinav2@users.noreply.github.com>
2022-04-27 08:54:06 +0530

    updated to calculator

--- Login.py
+++ Login.py
@@ -1,24 +1,18 @@
-def numbers_to_strings(argument):
+def numbers_to_strings(a,b,s):
     switcher = {
-        0: "zero",
-        1: "one",
-        2: "two",
-        3: "Three",
-        4: "Four",
-        5: "Five",
-        6: "Six",
-        7: "Seven",
-        8: "eight",
-        9: "Nine",
-        10: "Ten",
+        add: print(a+b),
+        sub: print(a-b),
+        div: print(a/b),
+        mul: print(a*b)
     }
     # get() method of dictionary data type returns
     # value of passed argument if it is present
     # in dictionary otherwise second argument will
     # be assigned as default value of passed argument
-    return switcher.get(argument, "nothing")
+    return switcher.get(s, "nothing")
 # Main program
 if __name__ == "__main__":
-    print("Enter your Switch case: ")
-    argument=int(input())
-    print (numbers_to_strings(argument))
+    print("Enter your numbers: ")
+    a,b=int(input()).split()
+    s=input()
+    print (numbers_to_strings(a,b,s))
