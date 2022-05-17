ef48a507c8deec8c42dbe1a3148d520e130eb67a
adityaabhinav2 <91604826+adityaabhinav2@users.noreply.github.com>
2022-05-17 19:29:28 +0530

    fix

--- Login.py
+++ Login.py
@@ -1,13 +1,8 @@
 """
 Python Code to install new packages
 """
-print("HII")
-print("Hello i am ")
-print("Raja i am working now")
-print("Started working on github")
-print("started rebase in git")
-
-'''
-going to start now
-'''
-
+"""
+matplotlib
+scikit-learn
+numpy
+"""
